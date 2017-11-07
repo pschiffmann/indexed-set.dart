0a35b880db48017237fc0350de472a6d2da7a1b6
Philipp Schiffmann <philippschiffmann93@gmail.com>
2017-11-07 12:21:44 +0100

    Add Subset tests

--- lib/src/subset.dart
+++ lib/src/subset.dart
@@ -3,13 +3,23 @@ import 'dart:collection' show SetMixin;
 import 'indexed_set.dart';
 import 'superset.dart';
 
+/// Returns the number of `1` bits in `byte`, which must not be negative.
+int _countBits(int byte) {
+  var count = 0;
+  while (byte > 0) {
+    count += byte & 1;
+    byte >>= 1;
+  }
+  return count;
+}
+
 /// A subset only contains elements from its [superset].
 ///
 /// Because the superset is immutable and assigns an index to each of its
-/// elements, this class can be implemented very space-efficient, using only 1
-/// bit per element in the superset. Set operations ([difference],
-/// [intersection], [union]) on subsets of the same superset benefit from this
-/// as well.
+/// elements, this class is very space-efficient, using only 1 bit per element
+/// in the superset. Set operations ([difference], [intersection], [union],
+/// [addAll], [removeAll], [retainAll]) on subsets of the same superset benefit
+/// from this as well.
 class Subset<E> extends SetMixin<E> implements IndexedSet<int, E> {
   /// This object dictates which elements this set can store, and provides its
   /// [index] and `isValidElement` functions.
@@ -17,21 +27,47 @@ class Subset<E> extends SetMixin<E> implements IndexedSet<int, E> {
 
   /// Each bit in this list indicates the presence (`1`) or absense (`0`) of the
   /// element with the corresponding index in [superset].
-  final Uint8List _elements;
+  Uint8List _elements;
+
+  /// Cached [length]. Kept in sync with [_elements] by all mutating methods,
+  /// and by [_updateLength].
+  int _length;
 
   /// Is incremented every time this set is changed (by [add] and [remove]).
-  /// Read by [_SubsetIterator] to detect and throw on concurrent modifications.
+  /// Read by [_SubsetIterator] to detect concurrent modifications.
   int _modificationCount = 0;
 
   @override
   Iterator<E> get iterator => new _SubsetIterator(this);
 
   @override
-  int get length => superset.length;
+  int get length => _length;
 
-  Subset(this.superset)
+  /// Creates a subset of `superset`.
+  ///
+  /// If `filled` is `true`, the set initially contains all elements from
+  /// `superset`. Else, it is initialized empty.
+  Subset(this.superset, {bool filled: false})
       : _elements = new Uint8List(
-            superset.length ~/ 8 + (superset.length % 8 > 0 ? 1 : 0));
+            superset.length ~/ 8 + (superset.length % 8 > 0 ? 1 : 0)),
+        _length = 0 {
+    if (filled) {
+      for (var i = 0; i < _elements.length - 1; i++) {
+        _elements[i] = 255;
+      }
+      if (_elements.isNotEmpty) {
+        // In the last byte, set only the bits to `1` that are actually used
+        // so the unused bits won't be counted by [_updateLength].
+        _elements[_elements.length - 1] = 255 >> (8 - superset.length % 8);
+      }
+      _length = superset.length;
+    }
+  }
+
+  Subset._copy(Subset<E> other)
+      : superset = other.superset,
+        _elements = new Uint8List.fromList(other._elements),
+        _length = other._length;
 
   @override
   bool add(E element) {
@@ -45,8 +81,29 @@ class Subset<E> extends SetMixin<E> implements IndexedSet<int, E> {
     }
     if (_checkElement(i)) return false;
     _setElement(i);
+    _length++;
     _modificationCount++;
     return true;
+  }
+
+  @override
+  void addAll(Iterable<E> elements) {
+    if (elements is Subset<E> && superset == elements.superset) {
+      for (var i = 0; i < _elements.length; i++) {
+        _elements[i] |= elements._elements[i];
+      }
+      _updateLength();
+      _modificationCount++;
+    } else {
+      super.addAll(elements);
+    }
+  }
+
+  @override
+  void clear() {
+    _elements = new Uint8List(_elements.length);
+    _length = 0;
+    _modificationCount++;
   }
 
   @override
@@ -59,6 +116,18 @@ class Subset<E> extends SetMixin<E> implements IndexedSet<int, E> {
 
   @override
   bool containsKey(int i) => superset.containsKey(i) && _checkElement(i);
+
+  @override
+  Subset<E> difference(Set<Object> other) => toSet()..removeAll(other);
+
+  @override
+  Subset<E> intersection(Set<Object> other) {
+    if (other is Subset<E> && superset == other.superset) {
+      return toSet()..retainAll(other);
+    } else {
+      return super.intersection(other);
+    }
+  }
 
   @override
   int index(E element) => superset.index(element);
@@ -77,12 +146,39 @@ class Subset<E> extends SetMixin<E> implements IndexedSet<int, E> {
     final i = index(object);
     if (i == -1 || !_checkElement(i)) return false;
     _unsetElement(i);
+    _length--;
     _modificationCount++;
     return true;
   }
 
   @override
-  Subset<E> toSet() => new Subset<E>(superset);
+  void removeAll(Iterable<Object> elements) {
+    if (elements is Subset<E> && superset == elements.superset) {
+      for (var i = 0; i < _elements.length; i++) {
+        _elements[i] &= ~elements._elements[i];
+      }
+      _updateLength();
+      _modificationCount++;
+    } else {
+      super.removeAll(elements);
+    }
+  }
+
+  @override
+  void retainAll(Iterable<Object> elements) {
+    if (elements is Subset<E> && superset == elements.superset) {
+      for (var i = 0; i < _elements.length; i++) {
+        _elements[i] &= elements._elements[i];
+      }
+      _updateLength();
+      _modificationCount++;
+    } else {
+      super.retainAll(elements);
+    }
+  }
+
+  @override
+  Subset<E> toSet() => new Subset<E>._copy(this);
 
   @override
   E operator [](int i) => containsKey(i) ? superset[i] : null;
@@ -95,13 +191,17 @@ class Subset<E> extends SetMixin<E> implements IndexedSet<int, E> {
 
   /// Sets the ith bit in [_elements] to `0`.
   void _unsetElement(int i) => _elements[i ~/ 8] &= ~(1 << i % 8);
+
+  /// Counts the number of set bits in [_elements] and updates [_length].
+  void _updateLength() =>
+      _length = _elements.fold(0, (count, byte) => count + _countBits(byte));
 }
 
 class _SubsetIterator<E> implements Iterator<E> {
   final Subset<E> _subset;
 
-  /// The index of [current] in [_subset]. `-1` indicates that the iterator is
-  /// uninitialized ([moveNext] was never called).
+  /// The index of [current] in [_subset.superset]. `-1` indicates that the
+  /// iterator is uninitialized ([moveNext] was never called).
   int _position = -1;
 
   /// The value of [_subset._modificationCount] at the time when this iterator
@@ -122,7 +222,7 @@ class _SubsetIterator<E> implements Iterator<E> {
     if (_modificationCount != _subset._modificationCount) {
       throw new ConcurrentModificationError(_subset);
     }
-    while (_position + 1 < _subset.length) {
+    while (_position + 1 < _subset.superset.length) {
       _position++;
       if (_subset._checkElement(_position)) {
         _current = _subset[_position];
---
+++ test/subset_test.dart
@@ -0,0 +1,286 @@
+import 'package:test/test.dart';
+import 'package:indexed_set/indexed_set.dart';
+
+void main() {
+  group("Subset", () {
+    final alphabet = new List.generate(
+        26, (n) => new String.fromCharCode(n + "A".codeUnitAt(0)));
+    final vowels = const ["A", "E", "I", "O", "U"];
+    final first10 = const ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+
+    // Taken for vowels∘first10, ∘∈{∩, ∪, \}
+    final difference = const ["O", "U"];
+    final intersection = const ["A", "E", "I"];
+    final union = const [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "O",
+      "U"
+    ];
+
+    final superset = new Superset<String>(alphabet);
+
+    Subset<String> subset;
+    setUp(() => subset = new Subset<String>(superset));
+
+    group("iterator", () {
+      test("visits all elements", () {
+        subset.addAll(vowels);
+        final it = subset.iterator;
+        for (var i = 0; i < vowels.length; i++) {
+          expect(it.moveNext(), isTrue);
+          expect(it.current, equals(vowels[i]));
+        }
+        expect(it.moveNext(), isFalse);
+      });
+
+      group("detects concurrent modifications by", () {
+        Iterator<String> it;
+        setUp(() {
+          subset.addAll(vowels);
+          it = subset.iterator..moveNext();
+        });
+
+        test("add()", () {
+          subset.add("X");
+          expect(it.moveNext, throwsConcurrentModificationError);
+          expect(it.current, equals("A"));
+        });
+
+        group("addAll() when called with", () {
+          test("Iterable", () {
+            subset.addAll(first10);
+            expect(it.moveNext, throwsConcurrentModificationError);
+            expect(it.current, equals("A"));
+          });
+
+          test("Subset", () {
+            subset.addAll(new Subset<String>(superset)..addAll(first10));
+            expect(it.moveNext, throwsConcurrentModificationError);
+            expect(it.current, equals("A"));
+          });
+        });
+
+        test("clear()", () {
+          subset.clear();
+          expect(it.moveNext, throwsConcurrentModificationError);
+          expect(it.current, equals("A"));
+        });
+
+        test("remove()", () {
+          subset.remove("A");
+          expect(it.moveNext, throwsConcurrentModificationError);
+          expect(it.current, equals("A"));
+        });
+
+        group("removeAll() when called with", () {
+          test("Iterable", () {
+            subset.removeAll(first10);
+            expect(it.moveNext, throwsConcurrentModificationError);
+            expect(it.current, equals("A"));
+          });
+
+          test("Subset", () {
+            subset.removeAll(new Subset<String>(superset)..addAll(first10));
+            expect(it.moveNext, throwsConcurrentModificationError);
+            expect(it.current, equals("A"));
+          });
+        });
+
+        group("retainAll() when called with", () {
+          test("Iterable", () {
+            subset.retainAll(first10);
+            expect(it.moveNext, throwsConcurrentModificationError);
+            expect(it.current, equals("A"));
+          });
+
+          test("Subset", () {
+            subset.retainAll(new Subset<String>(superset)..addAll(first10));
+            expect(it.moveNext, throwsConcurrentModificationError);
+            expect(it.current, equals("A"));
+          });
+        });
+      });
+    });
+
+    group("constructor", () {
+      test("creates empty set with filled=false", () {
+        expect(subset, isEmpty);
+      });
+
+      test("creates set containing all elements from superset with filled=true",
+          () {
+        expect(new Subset(superset, filled: true), unorderedEquals(superset));
+      });
+    });
+
+    group("add()", () {
+      test("adds missing element", () {
+        expect(subset.add("A"), isTrue);
+        expect(subset.length, equals(1));
+      });
+
+      test("rejects elements not in the superset", () {
+        expect(() => subset.add("a"), throwsArgumentError);
+        expect(subset.contains("a"), isFalse);
+        expect(subset.length, equals(0));
+      });
+
+      test("returns false if the element is already in the set", () {
+        subset.add("A");
+        expect(subset.add("A"), isFalse);
+        expect(subset.length, equals(1));
+      });
+    });
+
+    group("addAll() adds all elements from the argument", () {
+      test("List", () {
+        subset.addAll(vowels);
+        subset.addAll(first10);
+        expect(subset, orderedEquals(union));
+        expect(subset.length, equals(union.length));
+      });
+
+      test("Subset", () {
+        subset.addAll(vowels);
+        subset.addAll(new Subset(superset)..addAll(first10));
+        expect(subset, orderedEquals(union));
+        expect(subset.length, equals(union.length));
+      });
+    });
+
+    test("clear() removes all elements", () {
+      subset.addAll(vowels);
+      subset.clear();
+      expect(subset.isEmpty, isTrue);
+    });
+
+    group("contains()", () {
+      test("returns true for elements that are in the set", () {
+        subset.addAll(vowels);
+        expect(subset.contains("A"), isTrue);
+      });
+
+      test("returns false for elements that are not in the set", () {
+        subset.addAll(vowels);
+        expect(subset.contains("B"), isFalse);
+      });
+
+      test("returns false for objects that are not in the superset", () {
+        subset.addAll(vowels);
+        expect(subset.contains(null), isFalse);
+        expect(subset.contains(65), isFalse);
+      });
+    });
+
+    group("containsKey()", () {
+      test("returns true for indexes that are in the set", () {
+        subset.addAll(vowels);
+        expect(subset.containsKey(0), isTrue);
+      });
+
+      test("returns false for indexes that are not in the set", () {
+        subset.addAll(vowels);
+        expect(subset.containsKey(1), isFalse);
+      });
+    });
+
+    group("intersection() returns the intersection when called with a", () {
+      test("Hashset", () {
+        subset.addAll(vowels);
+        final i = subset.intersection(new Set.from(first10));
+        expect(i, orderedEquals(intersection));
+        expect(i.length, equals(intersection.length));
+      });
+
+      test("Subset", () {
+        subset.addAll(vowels);
+        final i = subset.intersection(new Subset(superset)..addAll(first10));
+        expect(i, orderedEquals(intersection));
+        expect(i.length, equals(intersection.length));
+      });
+    });
+
+    group("lookup()", () {
+      test("finds an element if it is in the set", () {
+        subset.addAll(vowels);
+        expect(subset.lookup("A"), equals("A"));
+      });
+
+      test("returns null if no equal element is in the set", () {
+        expect(subset.lookup("A"), isNull);
+      });
+    });
+
+    group("remove()", () {
+      test("removes an element if it is in the set", () {
+        subset.addAll(vowels);
+        expect(subset.remove("A"), isTrue);
+        expect(subset.contains("A"), isFalse);
+        expect(subset.length, equals(4));
+      });
+
+      test("returns false if the object was not in the set", () {
+        expect(subset.remove("A"), isFalse);
+        expect(subset.length, equals(0));
+      });
+
+      test("returns false if the object can't be in the set", () {
+        expect(subset.remove(4), isFalse);
+        expect(subset.length, equals(0));
+      });
+    });
+
+    group("removeAll() removes all elements when called with a", () {
+      test("Hashset", () {
+        subset
+          ..addAll(vowels)
+          ..removeAll(new Set.from(first10));
+        expect(subset, orderedEquals(difference));
+        expect(subset.length, equals(difference.length));
+      });
+
+      test("Subset", () {
+        subset
+          ..addAll(vowels)
+          ..removeAll(new Subset(superset)..addAll(first10));
+        expect(subset, orderedEquals(difference));
+        expect(subset.length, equals(difference.length));
+      });
+    });
+
+    group("retainAll() removes all other elements when called with a", () {
+      test("Hashset", () {
+        subset
+          ..addAll(vowels)
+          ..retainAll(new Set.from(first10));
+        expect(subset, orderedEquals(intersection));
+        expect(subset.length, equals(intersection.length));
+      });
+
+      test("Subset", () {
+        subset
+          ..addAll(vowels)
+          ..retainAll(new Subset(superset)..addAll(first10));
+        expect(subset, orderedEquals(intersection));
+        expect(subset.length, equals(intersection.length));
+      });
+    });
+
+    test("toSet() creates exact copy", () {
+      subset.addAll(vowels);
+      final copy = subset.toSet();
+      expect(copy, const isInstanceOf<Subset>());
+      expect(copy, orderedEquals(subset));
+      expect(copy.length, equals(5));
+    });
+  });
+}
